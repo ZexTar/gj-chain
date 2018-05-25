d07ba9af52becccb30737fcdc219b8b2bb618c74
ZexTar <borisstodickg@gmail.com>
2018-05-25 21:27:13 +0200

    adding ecdsa

--- dev-test.js
+++ dev-test.js
@@ -1,17 +1,4 @@
-const Blockchain = require('./blockchain');
+const  Wallet = require('./wallet');
 
-//testing block creation
-
-// const block = new Block('tihi', 'prle', 'mrki', 'pufta');
-// console.log(block.toAscii());
-// console.log(Block.genesis().toAscii());
-
-//testing mine function
-
-const bc = new Blockchain();
-
-for (let i = 0; i < 60; i++){
-	console.log(bc.addBlock(`cigan${i}`));
-	if (bc.chain[i-1] !== undefined)
-	console.log(bc.chain[i].timestamp - bc.chain[i-1].timestamp);
-}
+const wallet = new Wallet();
+console.log(wallet.toAscii());
--- wallet/index.js
+++ wallet/index.js
@@ -1,16 +1,18 @@
-const { INITIAL_BALANCE } = require('../config');
+const { INITIAL_BALANCE } = require('../conf');
+const ChainUtil = require('../chain-util');
 
 class Wallet {
 	constructor() {
 		this.balance = INITIAL_BALANCE;
-		this.keyPair = null; 
-		this.publicKey = null;
+		this.keyPair = ChainUtil.genKeyPair(); 
+		this.publicKey = this.keyPair.getPublic().encode('hex');
 	}
 
-	toAscii {
+	toAscii() {
+		
 		return `Wallet - 
-		publicKey:${this.publicKey.toString()}
-		balance  :${this.balance}`
+	publicKey:${this.publicKey.substring(0, 20)}
+	balance  :${this.balance}`
 	}
 }
 
