5d4964be953a4c147a5c0f12742e8e7b24b73413
ZexTar <borisstodickg@gmail.com>
2018-07-20 18:08:16 +0200

    handle transaction message in p2p server

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
 
-
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
@@ -17,27 +16,32 @@ const p2pServer = new P2pServer(bc, tp);
 app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
-	res.json(bc.chain);
+  res.json(bc.chain);
 });
 
 app.post('/mine', (req, res) => {
-	const block = bc.addBlock(req.body.data);
-	console.log(`New block added: ${block.toString()}`);
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
 
-	p2pServer.syncChains();
+  p2pServer.syncChains();
 
-	res.redirect('/blocks');
-}); 
+  res.redirect('/blocks');
+});
 
 app.get('/transactions', (req, res) => {
-	res.json(tp.transactions);
+  res.json(tp.transactions);
 });
 
 app.post('/transact', (req, res) => {
-	const { recipient, amount } = req.body;
-	const transaction = wallet.createTransaction(recipient, amount, tp);
-	res.redirect('/transactions');
-})
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  p2pServer.broadcastTransaction(transaction);
+  res.redirect('/transactions');
+});
+
+app.get('/public-key', (req, res) => {
+  res.json({ publicKey: wallet.publicKey });
+});
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pServer.listen();
